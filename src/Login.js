5ebf389d512ba1dac376659c13903ade429398c1
ejembi22 <ejembi_emma@icloud.com>
2025-09-26 03:56:08 -0700

    connecting backend

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,122 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 
 const Login = () => {
+    const [formData, setFormData]=useState({email: "", password: ""});
+    const [error, setError]=useState("");
+    const [success, setSuccess]=useState("")
+    const [loading, setLoading]=useState(false);
+    const navigate = useNavigate();
+
+    //handle input changes
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+    //handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        if(!formData.email || !formData.password){
+            return setError("Email and Password are required");
+        }
+
+        try{
+            setLoading(true);
+
+            const response = await fetch("https://sage-api-o3hl.onrender.com/api/auth", {
+            // const response = await fetch("/api/auth", {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+
+                body:JSON.stringify(formData),
+
+            });
+
+            const data = await response.json();
+
+            // let data;
+
+            // try{
+                
+            //      data = await response.json();
+            // } catch {
+            //     throw new Error("Server returned an invalid response");
+            // }
+
+            
+
+            if(!response.ok){
+                throw new Error(data.message || "Login failed");
+            }
+
+            // save token
+            if(data.token){
+                localStorage.setItem("authToken", data.token);
+            }
+
+            setSuccess("Login Successful");
+            console.log("Login Success", data);
+
+           setTimeout(() => navigate("/dashboard"), 2000) ; //link the dashboard here once it is ready
+        } catch (error){
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
   return (
     <div className='logincontainer'>
         <p className='logintext text-center'>Login to your account</p>
         <div className='loginformcontainer'>
 
-        <form id='loginform'>
-         <label for='email' className='loginemail'>Email Address</label>  <br/> 
-         <input type='email' id='loginemail' placeholder='Enter your email' className='logininputfield mt-2' required /> <br/>
+        <form id='loginform' onSubmit={handleSubmit}>
+         <label htmlFor='email' className='loginemail'>Email Address</label>  <br/> 
+         <input type='email' id='loginemail' name='email' value={formData.email} onChange={handleChange} placeholder='Enter your email' className='logininputfield mt-2' required /> <br/>
 
-         <label for='password' className='loginpassword pt-5'>Password</label> <br/>
-         <input type='password' id='loginpassword' placeholder='Enter your Password' className='logininputfield mt-2' required /> <br/>
+         <label htmlFor='password' className='loginpassword pt-5'>Password</label> <br/>
+         <input type='password' id='loginpassword' name='password' value={formData.password} onChange={handleChange} placeholder='Enter your Password' className='logininputfield mt-2' required /> <br/>
+
+         {error && <p style={{color: "red"}}>{error}</p>}
+         {success && <p style={{color:"green"}}>{success}</p>}
+
+
+         <button
+            id="loginbtn"
+            type="submit"
+            className="loginbtn"
+            disabled={loading}
+          >
+            {loading ? "Logging in..." : "Login" }
+           
+          </button>
 
 
          
 
         </form>
-         <button
-            id="loginbtn"
-            type="submit"
-        
-            className="loginbtn"
-          >
-           Login
-          </button>
+
+        {success && (
+            <div style= {{
+                backgroundColor: "White",
+                color: "green",
+                padding:"10px",
+                borderRadius:"5px",
+                textAlign:"center",
+                marginTop: "10px",
+                fontWeight: "bold"
+            }}>
+                {success}
+
+
+            </div>
+        )}
+         
 
            <div class="text-center py-4">
             <Link to={'/signup'} class="text-decoration-none fst-italic"
--- src/Signup.js
+++ src/Signup.js
@@ -1,40 +1,123 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 
 const Signup = () => {
+
+  // State for form inputs
+  const [formData, setFormData] = useState({
+    name:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+
+  });
+
+  const [error, setError] = useState(""); // To show error messages
+  const [success, setSuccess] = useState(""); // To show success messages
+  const [loading, setLoading] = useState(false); // Button loading state
+
+  // Handle input changes
+  const handleChange = (e) =>{
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  //Handle form submission 
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+
+    // Basic Validation 
+    if(!formData.name || !formData.email || !formData.password || !formData.confirmPassword){
+      return setError("All fields are required");
+    }
+
+    if (formData.password.length < 6){
+      return setError("Password must be at least 6 characters long");
+    }
+
+    if (formData.password !==  formData.confirmPassword){
+      alert("Passwords do not match")
+
+    }
+
+    try{
+      setLoading(true);
+
+      //call API
+      const response = await fetch("https://sage-api-o3hl.onrender.com/api/users", {
+
+      // const response = await fetch("/api/users", {
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name:formData.name,
+          email:formData.email,
+          password:formData.password
+        })
+      });
+      const data = await response.json();
+
+      if(!response.ok) {
+        //Handle API errors safely
+        throw new Error(data.message || "Failed to create account");
+      }
+      console.log("Account created successfully:", data) //log my response on console
+      setSuccess("Account created successfully");
+      setFormData({ name: "", email: "", password: "", confirmPassword: ""});
+    } catch (error){
+      setError(error.message);
+    }finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className='createaccountcontainer'>
         <p className='createyouraccounttext text-center'>Create your account</p>
 
         <div className='createyouraccountcontainer'>
 
-          <form id='createyouraccountform'> 
-            <label for='name' className='createaccountdetailstext'>Full Name</label> <br/>
-            <input type='text' id='fullname' name='name' placeholder='Enter your full name' className='createinputfield mt-2' required/> <br/>
+          {/* Attach onSubmit to form */}
 
-            <label for='email' className='createaccountdetailstext'>Email Address</label><br/>
-            <input type='email' id='createemail' placeholder='Enter your email' className='createinputfield mt-2' required /> <br/>
+          <form id='createyouraccountform' onSubmit={handleSubmit}> 
+            <label htmlFor='name' className='createaccountdetailstext'>Full Name</label> <br/>
+            <input type='text' id='fullname' name='name' placeholder='Enter your full name' className='createinputfield mt-2' value={formData.name} onChange={handleChange} required/> <br/>
 
-            <label for='password' className='createaccountdetailstext'>Password</label><br/>
-            <input type='password' id='createpassword' className='createinputfield mt-2' placeholder='Create your password ' required/> <br/>
+            <label htmlFor='email' className='createaccountdetailstext'>Email Address</label><br/>
+            <input type='email' id='createemail' name='email' placeholder='Enter your email' className='createinputfield mt-2' value={formData.email} onChange={handleChange} required /> <br/>
 
-            <label for='password' className='createaccountdetailstext' >Confirm Password</label><br/>
-            <input type='password' id='confirmpassword' className='createinputfield mt-2' placeholder='Confirm your password' required/>
+            <label htmlFor='password' className='createaccountdetailstext'>Password</label><br/>
+            <input type='password' id='createpassword' name='password' className='createinputfield mt-2' placeholder='Create your password ' value={formData.password} onChange={handleChange} required/> <br/>
+
+            <label htmlFor='confirmpassword' className='createaccountdetailstext' >Confirm Password</label><br/>
+            <input type='password' id='confirmpassword' name='confirmPassword' className='createinputfield mt-2' placeholder='Confirm your password' value={formData.confirmPassword} onChange={handleChange} required/> <br/>
+
+            {/* set error/success messages */}
+            {error && <p style={{color: "red"}}>{error}</p>}
+            {success && <p style={{color:"green"}}>{success}</p>}
+
+
+             <button
+            id="signupbtn"
+            type="submit"
+        
+            className="signupbtn"
+            disabled={loading}
+          >
+            {loading ? "signing up ...": "Sign Up"}
+           
+          </button>
 
 
 
 
           </form>
 
-          <button
-            id="loginbtn"
-            type="submit"
-        
-            className="signupbtn"
-          >
-           Sign Up
-          </button>
+         
 
            <div class="text-center py-4">
             <Link to={'/login'} class="text-decoration-none fst-italic"
